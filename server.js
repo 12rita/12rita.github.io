53a8cc0707ee11a8657175218950ed43a3b7a907
Margo <12rita43@gmail.com>
2020-02-08 16:34:50 +0300

    new server

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 
 const http = require("http");
 
+const server = http.createServer((req, res) => {
+    let data = [];
+    req.on('data', chunk => {
+        data.push(chunk)
+
+    });
+    req.on('end', () => {
+        JSON.parse(data)
+    });
+    res.end();
+});
+server.listen(3000);
+
 // let Server=new http.Server();
 // server.on('request', function (rew,res) {
 // re.end('hello');
 // })
 
-http.createServer(function(request, response){
+// http.createServer(function(req, res){
+//
+//     let data = [];
+//     req.on('data', chunk => {
+//         data.push(chunk)
+//     });
+//     req.on('end', () => {
+//         JSON.parse(data)
+//     });
+// // console.log(request.headers);
+// //     response.write('<h2>hello world</h2>');
+//     console.log(data);
+//     res.end();
+//
+// }).listen(3000);
 
-console.log(request.body);
-    response.write("<h2>hello world</h2>");
-    response.end();
-
-}).listen(3000);
