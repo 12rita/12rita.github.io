6c8aff44773c07cd1eaee77ff9917817bcebd07e
Margo <12rita43@gmail.com>
2020-02-08 21:34:05 +0300

    working aa!

--- server.js
+++ server.js
@@ -1,6 +1,56 @@
 
 const http = require("http");
+fs = require('fs');
 
+// language=HTML
+var body = `<html>
+<head>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            font-family: 'Lato', sans-serif;
+            text-align: center;
+        }
+
+        .text {
+            margin-top: 20%;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 48px;
+            line-height: 58px;
+            color: #FFFFFF;
+        }
+
+        .button {
+            background: #FFFFFF;
+            border-radius: 6px;
+            width: 300px;
+            height: 100px;
+            font-size: 30px;
+            line-height: 58px;
+        }
+
+    </style>
+    <meta charset="utf-8">
+</head>
+<body bgcolor="00AEAF">
+<div class='block'>
+    <p class='text'>
+        Что ж, неплохо, но ты же не думал, что на этом всё закончится?
+    </p>
+    <br>
+
+</div>
+<br>
+<br>
+<a href="./step1.html">
+    <input class='button' type="button" value="Начать страдать">
+</a>
+
+</body>
+</html>`;
 const server = http.createServer((req, res) => {
     let data = [];
     req.on('data', chunk => {
@@ -8,9 +58,26 @@ const server = http.createServer((req, res) => {
 
     });
     req.on('end', () => {
-        JSON.parse(data)
+
+        data = Buffer.concat(data).toString();
+        let pass=data.split('=');
+        let password=pass[1];
+        if (password==='374'){
+
+            res.writeHead(200,{"Content-Type" : "text/html"});
+            res.write(body);
+
+            res.end();
+        }
+        else{
+            res.write('nooo');
+            res.end();
+        }
+
+
     });
-    res.end();
+
+
 });
 server.listen(3000);
 
